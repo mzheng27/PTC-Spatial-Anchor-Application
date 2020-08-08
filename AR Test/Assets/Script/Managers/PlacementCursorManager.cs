0f14d2e0c25d54799cb73074190541f60762707d
mzheng27 <59420248+mzheng27@users.noreply.github.com>
2020-08-08 15:02:55 -0400

    Text w. Cube (Local Anchor)
     Correction to last commit

--- AR Test/Assets/Script/Managers/PlacementCursorManager.cs
+++ AR Test/Assets/Script/Managers/PlacementCursorManager.cs
@@ -1,74 +1,79 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.Experimental.XR;
 using UnityEngine.XR.ARSubsystems;
-using System;
-using System.Diagnostics.Eventing.Reader;
-using System.Security.Cryptography;
+using UnityEngine.UI;
 
 public class PlacementCursorManager : MonoBehaviour
 {
+    private AppStateManager appStateManager;
+    private GeneralConfiguration generalConfiguration;
     private ARRaycastManager arRaycastManager;
-    //the position and rotation of the point
-    private Pose placementPosition;
-    private bool isValidPlacement = false;
-    public GameObject placementMark;
-    public GameObject objectToPlace;
-    public bool enabled;
-    public GameObject cube;
-    void Start()
+    private GameObject placementCursor;
+    //public GameObject objectToPlace;
+    private ARReferencePointManager anchorManager;
+    private List<ARReferencePoint> anchors = new List<ARReferencePoint>();
+
+    //[SerializeField]
+    //private Canvas MobileUX;
+
+    //[SerializeField]
+    //private GameObject DragDropBar;
+
+
+    void Awake()
     {
+        appStateManager = FindObjectOfType<AppStateManager>();
+        generalConfiguration = FindObjectOfType<GeneralConfiguration>();
         arRaycastManager = FindObjectOfType<ARRaycastManager>();
+        placementCursor = Instantiate(generalConfiguration.placementCursorPrefab) as GameObject;
+        anchorManager = FindObjectOfType<ARReferencePointManager>();
+
     }
-    // Update is called once per frame
+
     void Update()
     {
-        if (enabled)
+        UpdateCursorPose();
+        UpdateCursorIndicator();
+        if (appStateManager.placementCursorIsSurface && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            UpdatePlacementPose();
-            UpdatePlacementMarkPosition();
-            if (isValidPlacement && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-            {
-                PlaceObject();
-            }
+            PlaceObject();
+        }
+
+    }
+
+    private void UpdateCursorPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var arRaycastHits = new List<ARRaycastHit>();
+        arRaycastManager.Raycast(screenCenter, arRaycastHits, TrackableType.Planes);
+        appStateManager.placementCursorIsSurface = arRaycastHits.Count > 0;
+        if (appStateManager.placementCursorIsSurface)
+        {
+            appStateManager.placementCursorPose = arRaycastHits[0].pose;
         }
     }
-    void UpdatePlacementPose()
-    {
-        var centerOfScreen = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0));
-        var hitList = new List<ARRaycastHit>();
-        //get a flat Surface to place the object
-        arRaycastManager.Raycast(centerOfScreen, hitList, TrackableType.Planes);
-        isValidPlacement = hitList.Count > 0;
 
-        if (isValidPlacement)
-        {
-            placementPosition = hitList[0].pose;
-            var cameraDirection = Camera.main.transform.forward;
-            var cameraRotation = new Vector3(cameraDirection.x, 0f, cameraDirection.z).normalized;
-            placementPosition.rotation = Quaternion.LookRotation(cameraRotation);
-        }
-    }
-    
-    private void UpdatePlacementMarkPosition()
+    private void UpdateCursorIndicator()
     {
-        if (isValidPlacement)
+        if (appStateManager.placementCursorIsSurface)
         {
-            placementMark.SetActive(true);
-            placementMark.transform.SetPositionAndRotation(placementPosition.position, placementPosition.rotation);
-
+            placementCursor.SetActive(true);
+            placementCursor.transform.SetPositionAndRotation(appStateManager.placementCursorPose.position, appStateManager.placementCursorPose.rotation);
         }
         else
         {
-            placementMark.SetActive(false);
+            placementCursor.SetActive(false);
         }
     }
-    
+
     public void PlaceObject()
     {
-        Instantiate(objectToPlace, placementPosition.position, placementPosition.rotation, cube.transform);
+        ARReferencePoint newAnchor = anchorManager.AddReferencePoint(appStateManager.placementCursorPose);
+        anchors.Add(newAnchor);
+        //DragDropBar.gameObject.SetActive(true);
     }
 }
+
 
