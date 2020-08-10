d35a67e5191a2cef25632c6f53264fa2b9b878c5
mzheng27 <59420248+mzheng27@users.noreply.github.com>
2020-08-10 02:49:53 -0400

    Selection w. Camera v1.0

---
+++ Unity/Assets/Script/Components/PlacementObject.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class PlacementObject : MonoBehaviour
+{
+    [SerializeField]
+    private bool IsSelected;
+
+
+
+    public bool Selected
+    {
+        get
+        {
+            return this.IsSelected;
+        }
+        set
+        {
+            IsSelected = value;
+        }
+    }
+
+   
+    [SerializeField]
+    private Canvas canvasComponent;
+
+   
+
+    public void ToggleCanvas()
+    {
+        canvasComponent?.gameObject.SetActive(IsSelected);
+    }
+}
