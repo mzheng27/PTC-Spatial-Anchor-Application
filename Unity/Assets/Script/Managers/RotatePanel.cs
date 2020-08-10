cb5fa707a36b85b79a73499643b86347a21e0a4e
JustinTaylor7 <60791586+JustinTaylor7@users.noreply.github.com>
2020-08-10 17:13:18 -0400

    Added screen

---
+++ Unity/Assets/Script/Managers/RotatePanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePanel : MonoBehaviour
+{
+    public GameObject Panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var facinguser = Camera.main.transform.position;
+        Panel.transform.LookAt(facinguser);
+    }
+}
