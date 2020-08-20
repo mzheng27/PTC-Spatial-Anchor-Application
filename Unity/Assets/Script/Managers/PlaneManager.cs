a3d97d3360e3666d6a10a396b948a86686906ceb
JustinTaylor7 <60791586+JustinTaylor7@users.noreply.github.com>
2020-08-20 09:39:10 -0400

    PlaneManager updated

---
+++ Unity/Assets/Script/Managers/PlaneManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PlaneManager : MonoBehaviour
+{
+    private int numplanes;
+    public GameObject startcanvas;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var planeManager = GetComponent<ARPlaneManager>();
+        foreach (var plane in planeManager.trackables)
+        {
+            numplanes++;
+        }
+
+        if (numplanes != 0)
+        {
+            startcanvas.gameObject.SetActive(true);
+        }
+    }
+}
